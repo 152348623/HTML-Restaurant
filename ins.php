1d489bffbc9fd4c1e9974e32f2d1b91fbbf065a9
baozi <std79645321@gmail.com>
2019-04-17 23:07:41 +0800

    no message

--- ins.php
+++ ins.php
@@ -1,5 +1,13 @@
-<?php
+<!-- <?php
 $sql = mysql_Connect("localhost","admin","1234","msg");
 $ins = sprintf("INSERT INTO msg_ (name,message) VALUES ('%%s%%','%%s%%')",$_GET["name"],$_GET["textarea"]);
 mysql_query($ins);
-?>
+?> -->
+<html>
+<body>
+
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is: <?php echo $_GET["textarea"]; ?>
+
+</body>
+</html>
---
+++ js/main.js
@@ -0,0 +1,12 @@
+/*$(function()
+{
+    $(".flexslider").flexslider(
+        {
+            slideshowSpeed: 5000, //展示时间间隔ms
+            animationSpeed: 500, //滚动时间ms
+            touch: true, //是否支持触屏滑动
+            direcionNav: true,
+            pauseOnAction:false
+        }
+    );
+});*/
